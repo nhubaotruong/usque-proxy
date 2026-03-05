8a4ae8fa9e527faaa155c2a882ce2df91134a543
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2026-03-05 10:40:23 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
